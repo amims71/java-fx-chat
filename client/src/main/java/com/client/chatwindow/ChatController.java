e4b662e8cb1e7284c3254e867895275532b179c9
Amimul Ehshan <amimulehshan29@gmail.com>
2021-03-24 02:46:37 +0600

    init

--- client/src/main/java/com/client/chatwindow/ChatController.java
+++ client/src/main/java/com/client/chatwindow/ChatController.java
@@ -1,20 +1,11 @@
 package com.client.chatwindow;
 
 import com.client.login.MainLauncher;
-import com.client.util.VoicePlayback;
-import com.client.util.VoiceRecorder;
-import com.client.util.VoiceUtil;
 import com.messages.Message;
-import com.messages.MessageType;
-import com.messages.Status;
 import com.messages.User;
 import com.messages.bubble.BubbleSpec;
 import com.messages.bubble.BubbledLabel;
-import com.traynotifications.animations.AnimationType;
-import com.traynotifications.notification.TrayNotification;
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
@@ -31,12 +22,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,18 +36,10 @@ public class ChatController implements Initializable {
 
     @FXML private TextArea messageBox;
     @FXML private Label usernameLabel;
-    @FXML private Label onlineCountLabel;
     @FXML private ListView userList;
     @FXML private ImageView userImageView;
-    @FXML private Button recordBtn;
     @FXML ListView chatPane;
-    @FXML ListView statusList;
     @FXML BorderPane borderPane;
-    @FXML ComboBox statusComboBox;
-    @FXML ImageView microphoneImageView;
-
-    Image microphoneActiveImage = new Image(getClass().getClassLoader().getResource("images/microphone-active.png").toString());
-    Image microphoneInactiveImage = new Image(getClass().getClassLoader().getResource("images/microphone.png").toString());
 
     private double xOffset;
     private double yOffset;
@@ -75,23 +54,6 @@ public class ChatController implements Initializable {
         }
     }
 
-    public void recordVoiceMessage() throws IOException {
-        if (VoiceUtil.isRecording()) {
-            Platform.runLater(() -> {
-                microphoneImageView.setImage(microphoneInactiveImage);
-                    }
-            );
-            VoiceUtil.setRecording(false);
-        } else {
-            Platform.runLater(() -> {
-                microphoneImageView.setImage(microphoneActiveImage);
-
-                    }
-            );
-            VoiceRecorder.captureAudio();
-        }
-    }
-
 
     public synchronized void addToChat(Message msg) {
         Task<HBox> othersMessages = new Task<HBox>() {
@@ -102,20 +64,12 @@ public class ChatController implements Initializable {
                 profileImage.setFitHeight(32);
                 profileImage.setFitWidth(32);
                 BubbledLabel bl6 = new BubbledLabel();
-                if (msg.getType() == MessageType.VOICE){
-                    ImageView imageview = new ImageView(new Image(getClass().getClassLoader().getResource("images/sound.png").toString()));
-                    bl6.setGraphic(imageview);
-                    bl6.setText("Sent a voice message!");
-                    VoicePlayback.playAudio(msg.getVoiceMsg());
-                }else {
-                    bl6.setText(msg.getName() + ": " + msg.getMsg());
-                }
+                bl6.setText(msg.getName() + ": " + msg.getMsg());
                 bl6.setBackground(new Background(new BackgroundFill(Color.WHITE,null, null)));
                 HBox x = new HBox();
                 bl6.setBubbleSpec(BubbleSpec.FACE_LEFT_CENTER);
                 x.getChildren().addAll(profileImage, bl6);
                 logger.debug("ONLINE USERS: " + Integer.toString(msg.getUserlist().size()));
-                setOnlineLabel(Integer.toString(msg.getOnlineCount()));
                 return x;
             }
         };
@@ -133,13 +87,7 @@ public class ChatController implements Initializable {
                 profileImage.setFitWidth(32);
 
                 BubbledLabel bl6 = new BubbledLabel();
-                if (msg.getType() == MessageType.VOICE){
-                    bl6.setGraphic(new ImageView(new Image(getClass().getClassLoader().getResource("images/sound.png").toString())));
-                    bl6.setText("Sent a voice message!");
-                    VoicePlayback.playAudio(msg.getVoiceMsg());
-                }else {
-                    bl6.setText(msg.getMsg());
-                }
+                bl6.setText(msg.getMsg());
                 bl6.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN,
                         null, null)));
                 HBox x = new HBox();
@@ -148,7 +96,6 @@ public class ChatController implements Initializable {
                 bl6.setBubbleSpec(BubbleSpec.FACE_RIGHT_CENTER);
                 x.getChildren().addAll(bl6, profileImage);
 
-                setOnlineLabel(Integer.toString(msg.getOnlineCount()));
                 return x;
             }
         };
@@ -172,9 +119,6 @@ public class ChatController implements Initializable {
         this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
     }
 
-    public void setOnlineLabel(String usercount) {
-        Platform.runLater(() -> onlineCountLabel.setText(usercount));
-    }
 
     public void setUserList(Message msg) {
         logger.info("setUserList() method Enter");
@@ -182,31 +126,8 @@ public class ChatController implements Initializable {
             ObservableList<User> users = FXCollections.observableList(msg.getUsers());
             userList.setItems(users);
             userList.setCellFactory(new CellRenderer());
-            setOnlineLabel(String.valueOf(msg.getUserlist().size()));
         });
         logger.info("setUserList() method Exit");
-    }
-
-    /* Displays Notification when a user joins */
-    public void newUserNotification(Message msg) {
-        Platform.runLater(() -> {
-            Image profileImg = new Image(getClass().getClassLoader().getResource("images/" + msg.getPicture().toLowerCase() +".png").toString(),50,50,false,false);
-            TrayNotification tray = new TrayNotification();
-            tray.setTitle("A new user has joined!");
-            tray.setMessage(msg.getName() + " has joined the JavaFX Chatroom!");
-            tray.setRectangleFill(Paint.valueOf("#2C3E50"));
-            tray.setAnimationType(AnimationType.POPUP);
-            tray.setImage(profileImg);
-            tray.showAndDismiss(Duration.seconds(5));
-            try {
-                Media hit = new Media(getClass().getClassLoader().getResource("sounds/notification.wav").toString());
-                MediaPlayer mediaPlayer = new MediaPlayer(hit);
-                mediaPlayer.play();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        });
     }
 
     public void sendMethod(KeyEvent event) throws IOException {
@@ -268,16 +189,6 @@ public class ChatController implements Initializable {
 
         borderPane.setOnMouseReleased(event -> {
             borderPane.setCursor(Cursor.DEFAULT);
-        });
-
-        statusComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                try {
-                    Listener.sendStatusUpdate(Status.valueOf(newValue.toUpperCase()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         });
 
         /* Added to prevent the enter from adding a new line to inputMessageBox */
